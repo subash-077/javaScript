fc41726f13d19cafb3fa865d7880e498be11f3ca
subash-077 <96123322+subash-077@users.noreply.github.com>
2022-02-01 10:41:13 +0530

    Add files via upload

---
+++ exampleOne.js
@@ -0,0 +1,3 @@
+console.log('chethan -software enginieer Im from js file ')
+document.write("hi chethan  from js file ")
+document.writeln("hi im shubash also from js file ")
---
+++ variables.js
@@ -0,0 +1,13 @@
+let a="nanu";
+let b="ninu";
+let c="hogona"; 
+let d="barge";
+console.log(a+" "+b+" "+c+" "+d)
+
+const cn=10;
+  //if const that becomes the final value
+console.log(cn)
+let value=symbol("book")
+let val=symbol("author")
+let library={book="Java",author="Chethan"};
+console.log(library)
